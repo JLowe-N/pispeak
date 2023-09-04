ce5a26d0a36c85f46620f65643106c0a2e6c4afa
Justin Lowen <Justin.G.Lowen@gmail.com>
2023-09-04 16:56:46 -0500

    Add text to speech and mp3 file annotation

---
+++ movemp3file.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import platform
+import shutil
+import os
+
+mp3name = 'faster.mp3'
+mp3destination = os.path.join(Path.home(), 'GoogleDrive', mp3name)
+shutil.move(mp3name, mp3destination)
